357b413d8f50c97cee42910ee51d2df9a0836ce0
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-12-06 12:38:31 -0500

    feat: add on-summary-create and is-owner middlewares for summary API

--- src/api/summary/routes/summary.ts
+++ src/api/summary/routes/summary.ts
@@ -2,6 +2,24 @@
  * summary router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi"
 
-export default factories.createCoreRouter('api::summary.summary');
+export default factories.createCoreRouter("api::summary.summary", {
+    config: {
+        create: {
+            middlewares: ["api::summary.on-summary-create"],
+        },
+        find: {
+            middlewares: ["global::is-owner"],
+        },
+        findOne: {
+            middlewares: ["global::is-owner"],
+        },
+        update: {
+            middlewares: ["global::is-owner"],
+        },
+        delete: {
+            middlewares: ["global::is-owner"],
+        },
+    },
+})
